d489997425154a3e3d5d491e9013f37cdfb96773
Vivek Adi <vivek.adishesha@gmail.com>
2025-04-02 12:44:30 +0800

    v2.6 update2

--- run_single.sh
+++ run_single.sh
@@ -2,15 +2,15 @@
 #PBS -j oe
 #PBS -l vnode=Boltzmann01
 #PBS -l walltime=24:00:00
-#PBS -l cudamem=2000mb
-#PBS -l ncpus=8
-#PBS -l mem=16gb
+#PBS -l cudamem=500mb
+#PBS -l ncpus=4
+#PBS -l mem=4gb
 
 . /etc/profile
 module load local mumax3
 
 export PROJDIR="/users/---/---/---/---/"
 
-cd ${PROJDIR} && $(which mumax3) stt_mram.mx3
+cd ${PROJDIR} && $(which mumax3) example2.mx3
 
 
